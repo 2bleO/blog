01f8c74a06de29a2e0611d19f71aab29854a514d
onesis <Onesis.olea@gmail.com>
2021-11-24 19:33:03 -0400

    method updates comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   has_many :posts
+
+  def update_comments_counter
+    post.update(comments_counter:Comment.where(post_id:post).count)
+  end
 end
