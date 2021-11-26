b6e64c2ca3ebf8ff9e28c63dab114756bb3542e4
onesis <Onesis.olea@gmail.com>
2021-11-26 01:23:26 -0400

    Add basic style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
-  def index;
+  def index
     @user = User.find(params[:user_id])
     @users = User.all
     @user_posts = @user.recent_posts
     @comments = Comment.all
   end
 
-  def show;
+  def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  def index;
+  def index
     @users = User.all
   end
 
-  def show;
+  def show
     @user = User.find(params[:id])
     @user_posts = Post.where(author_id: @user.id)
     @comments = Comment.all
