0d339992f1525790d09664d6b241cbde32fc0d85
onesis <Onesis.olea@gmail.com>
2021-11-26 04:04:38 -0400

    Refactor as per code review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user_posts = Post.where(author_id: @user.id)
-    @comments = Comment.all
+    @user_posts = Post.where(author_id: @user.id).includes(:comments)
   end
 end
