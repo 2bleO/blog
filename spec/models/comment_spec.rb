98d6e6dc517d01e090eea910eebd09e4c092be2b
onesis <Onesis.olea@gmail.com>
2021-11-26 23:15:54 -0400

    fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe '#update_counter' do
-  random = Random.new
-  users = User.all
-  posts = Post.all
-  subject { Comment.create(text: "Comment #1", post: posts[random.rand(posts.count)], author: users[random.rand(users.count)]) }
+    random = Random.new
+    users = User.all
+    posts = Post.all
+    subject do
+      Comment.create(text: 'Comment #1', post: posts[random.rand(posts.count)], author: users[random.rand(users.count)])
+    end
 
     it 'should update the post comments counter' do
       expect(subject.post.comments_counter).to be(1)
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe '#update_counter' do
-  subject { Post.new(title: 'Post title', text: 'Long lorem ipsum text', author: User.first) }
+    subject { Post.new(title: 'Post title', text: 'Long lorem ipsum text', author: User.first) }
 
     it 'should update the post likes counter' do
       subject.increment!(:likes_counter)
