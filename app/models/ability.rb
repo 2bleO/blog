a35af1b771aefbe38eb4a52b557782cf057bd7a0
onesis <Onesis.olea@gmail.com>
2021-12-02 00:23:49 -0400

    fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-      if user.role == 'admin'
-        can :manage, :all
-      else
-        can :manage, Post, author_id: user.id
-        can :manage, Comment, author_id: user.id
-        can :read, Post
-        can :read, Comment
-      end
-
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, author_id: user.id
+      can :read, Post
+      can :read, Comment
     end
   end
+end
