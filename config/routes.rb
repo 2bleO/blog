9fb43100f1e4c3e365d03b3d34b959d45dee7118
onesis <Onesis.olea@gmail.com>
2021-12-03 23:00:59 -0400

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   end
 
   resources :likes, only: %i[create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[index new create destroy]
+    end
+  end
 end
